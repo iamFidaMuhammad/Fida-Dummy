d6d46339c4679dd77c29fbac0ce6f86024efff53
Fida Muhammad <119914349+iamFidaMuhammad@users.noreply.github.com>
2022-12-06 10:18:44 +0500

    This fixes #2 issue
    On the Demand of @HammadMaqbool I added If-else program with this commit.

---
+++ if-else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 10;
+	int b = 20;
+	if(a==b)
+	{
+		cout<<"Both values are equal";
+	}
+	else
+	{
+		cout<<"Both values are not equal";
+	}
+
+}
+//Coded by Fida Muhammad
